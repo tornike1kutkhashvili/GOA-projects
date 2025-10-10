d1192b363faeac8ecef1a6f4516d94716e025069
tornike1kutkhashvili <kutkhashvilitornike91@gmail.com>
2025-10-10 18:51:25 +0400

    second commit

---
+++ day1/classwork1.py
@@ -0,0 +1,1 @@
+print("tornike kutkhashvili")
---
+++ day1/homework1.py
@@ -0,0 +1,20 @@
+
+
+my_name = "tornike"
+
+my_username = "kutkhashvili"
+
+my_age = (15)
+
+my_height = ("170cm")
+
+
+
+print(f"my name is {my_name}")
+
+print(f"my username is {my_username}")
+
+print(f"my age is {my_age}")
+
+print(f"my height is {my_height}")
+
