f0d640b1667ef70386f934eb2f9055908e7b14e5
tornike1kutkhashvili <kutkhashvilitornike91@gmail.com>
2025-10-13 21:56:29 +0400

    thrid commit

---
+++ day2/homework2/hw.4.py
@@ -0,0 +1,11 @@
+number1 = (7)
+
+number2 = (7)
+
+print(number1 + number2)
+
+print(number1 * number2)
+
+print(number2 / number1)
+
+print(number1 - number2)
---
+++ day2/homework2/hw6.py
@@ -0,0 +1,18 @@
+# მოიძიეთ ინფორმაცია კონკადინაციის შესახებ პითონში, 
+# თქვენი დავალებაა რომ შექმნათ რაიმოდენიმე ცვლადი მაგალითად:
+# თქვენი სახელის გვარის ასაკის სიმაღლის ქალაქის სადაც ცხოვრობთ და ასე შემდეგ.
+# ამ ცვლადების გამოყენებით დაბეჭდეთ ერთი მთლიანი წინადადება.
+
+
+my_name = ("Tornike")
+
+my_surname = ("Kutkhashvili")
+
+city = ("Rustavi")
+
+my_height = 1.72
+
+my_age = 15
+
+
+print(f"my name is {my_name} {my_surname}. my height is {my_height} i am {my_age} years old and i live in {city}")
