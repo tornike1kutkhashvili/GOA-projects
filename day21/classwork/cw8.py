e3b99c745cfb604e6bbdbf047b5c9e95d87b6d3a
tornike1kutkhashvili <kutkhashvilitornike91@gmail.com>
2026-03-06 00:51:48 +0400

    twentyfirst commit

---
+++ day21/classwork/cw8.py
@@ -0,0 +1,5 @@
+# 7)რატომ გამოაქვს ერორი ამ კოდს
+# int = "20"
+# print(int + 9)
+
+# imitom rom int cvladshi ricxvi chasmulia frchxilebshi da aris stringi da stringebsa da intejers shoris mimateba sheudzlebelia.
---
+++ day21/homework/hw2.py
@@ -0,0 +1,14 @@
+# 2)დაწერეთ ყველა ფუნქციაზე 3-3 მაგალითი
+
+# 1
+print("Hello")
+print("My name is Luka")
+print(5 + 10)
+# 2
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+surname = input("Enter your surname: ")
+# 3
+age = int("20")
+number = int("50")
+year = int("2024")
